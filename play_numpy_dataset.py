cce7aa25ff1c4d75f06ccd867c7c759ba9c924f8
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-07-31 15:03:14 -0400

    fixed frozen frame

--- play_numpy_dataset.py
+++ play_numpy_dataset.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-input_file_path = '/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/final_processed_data_3_channels_gamma.npz'
+input_file_path = '/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/data/final_processed_data.npz'
 npzfile = np.load(input_file_path)
 
 train_predictors = npzfile['train_predictors']
@@ -13,3 +13,7 @@ for frame_index, frame in enumerate(train_predictors):
     print(frame_index)
     cv2.imshow('frame', frame)
 
+    # This line is necessary or else it will look like the frames are frozen
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    print()
