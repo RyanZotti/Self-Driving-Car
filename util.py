4e37fc7750b8a95464577a0c1673ab3b3c31a3b6
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-08-07 15:57:01 -0400

    simplified func

--- util.py
+++ util.py
@@ -11,14 +11,17 @@ def dir_count(dir):
 def mkdir(dir):
     shell_cmd = 'mkdir -p {dir}'.format(dir=dir)
     subprocess.check_output(shell_cmd, shell=True).strip()
+    return dir
 
 
-def mkdir_tensorobard_dir(tensorboard_basedir):
-    old_run_index = int(dir_count(tensorboard_basedir))
+def mkdir_tfboard_run_dir(tf_basedir,):
+    old_run_index = int(dir_count(tf_basedir))
     new_run_index = str(old_run_index + 1)
-    mkdir(tensorboard_basedir + new_run_index+'/train/')
-    mkdir(tensorboard_basedir + new_run_index + '/validation/')
+    new_run_dir = tf_basedir + new_run_index
+    mkdir(new_run_dir)
+    return new_run_dir
+
 
 if __name__ == '__main__':
     tensorboard_basedir = '/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/tf_visual_data/runs/'
-    mkdir_tensorobard_dir(tensorboard_basedir)
+    mkdir_tfboard_run_dir(tensorboard_basedir)
