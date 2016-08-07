8fbe6adc1cca18ccf12361d06700c92ebcb4b96f
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-08-07 14:20:00 -0400

    util script for making dirs

---
+++ util.py
@@ -0,0 +1,25 @@
+import subprocess
+
+
+def dir_count(dir):
+    shell_cmd = 'ls -ltr {dir} | wc -l'.format(dir=dir)
+    cmd_result = subprocess.check_output(shell_cmd, shell=True).strip()
+    cmd_result = str(cmd_result).replace('b', '').replace("\'", "")
+    print(cmd_result)
+    return cmd_result
+
+
+def mkdir(dir):
+    shell_cmd = 'mkdir -p {dir}'.format(dir=dir)
+    subprocess.check_output(shell_cmd, shell=True).strip()
+
+
+def mkdir_tensorobard_dir(tensorboard_basedir):
+    old_run_index = int(dir_count(tensorboard_basedir))
+    new_run_index = str(old_run_index + 1)
+    mkdir(tensorboard_basedir + new_run_index+'/train/')
+    mkdir(tensorboard_basedir + new_run_index + '/validation/')
+
+if __name__ == '__main__':
+    tensorboard_basedir = '/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/tf_visual_data/runs/'
+    mkdir_tensorobard_dir(tensorboard_basedir)
