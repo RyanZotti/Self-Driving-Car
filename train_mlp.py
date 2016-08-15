2706aee627d30afacf0a665692170641ca57c9eb
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-08-14 21:20:06 -0400

    mlp consistly 43.2 percent accurate

--- train_mlp.py
+++ train_mlp.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import random
+from util import mkdir_tfboard_run_dir,mkdir,shell_command
+import os
 
 '''
 Helpful notes
@@ -71,13 +73,21 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# To view graph: tensorboard --logdir=/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/tf_visual_data/runs/1/
+# To view graph: tensorboard --logdir=/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/tf_visual_data/runs
 tf.scalar_summary('accuracy', accuracy)
 merged = tf.merge_all_summaries()
-tfboard_dir = '/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/tf_visual_data/runs/4/'
-train_writer = tf.train.SummaryWriter(tfboard_dir+"/train/",sess.graph)
-validation_writer = tf.train.SummaryWriter(tfboard_dir+"/validation/",sess.graph)
 
+tfboard_basedir = '/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/tf_visual_data/runs/'
+tfboard_run_dir = mkdir_tfboard_run_dir(tfboard_basedir)
+train_dir = mkdir(tfboard_run_dir+"/trn/mlp/")
+validation_dir = mkdir(tfboard_run_dir+"/vld/mlp/")
+
+# Archive this script to document model design in event of good results that need to be replicated
+model_file_path = os.path.dirname(os.path.realpath(__file__))+'/'+os.path.basename(__file__)
+shell_command('cp {model_file} {archive_path}'.format(model_file=model_file_path,archive_path=tfboard_run_dir+'/'))
+
+train_writer = tf.train.SummaryWriter(train_dir,sess.graph)
+validation_writer = tf.train.SummaryWriter(validation_dir,sess.graph)
 
 sess.run(tf.initialize_all_variables())
 batch_index = 0
@@ -122,3 +132,6 @@ for i in range(1000):
 saver = tf.train.Saver()
 save_path = saver.save(sess, "/Users/ryanzotti/Documents/repos/Self-Driving-Car/trained_model/model.ckpt")
 #print("validation accuracy %g" % accuracy.eval(feed_dict={x: validation_predictors, y_: validation_targets, keep_prob: 1.0}))
+
+# Marks unambiguous successful completion to prevent deletion by cleanup script
+shell_command('touch '+tfboard_run_dir+'/SUCCESS')
