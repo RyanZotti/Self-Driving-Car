30e27258b3763558ca00c3031a54d664d1ec8f39
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-08-28 15:23:15 -0400

    minor cleanup

--- play_numpy_dataset.py
+++ play_numpy_dataset.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-input_file_path = '/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/data/final_processed_data.npz'
+input_file_path = '/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/final_processed_data_3_channels.npz'
 npzfile = np.load(input_file_path)
 
 train_predictors = npzfile['train_predictors']
@@ -10,10 +10,9 @@ train_targets = npzfile['train_targets']
 print(train_predictors.shape)
 
 for frame_index, frame in enumerate(train_predictors):
-    print(frame_index)
+
     cv2.imshow('frame', frame)
 
     # This line is necessary or else it will look like the frames are frozen
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    print()
