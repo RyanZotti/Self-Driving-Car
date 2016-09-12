396565bf8d71a7ed221a77dcbeb408ce5062980a
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-09-12 19:16:37 -0400

    display arrow keys during live run

--- mlp_drive.py
+++ mlp_drive.py
@@ -5,6 +5,7 @@ import numpy as np
 from datetime import datetime
 import cv2
 import json
+import os
 
 sess = tf.InteractiveSession(config=tf.ConfigProto())
 
@@ -47,6 +48,11 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+image_path = str(os.path.dirname(os.path.realpath(__file__))) + "/arrow_key_images"
+up_arrow = cv2.imread(image_path + '/UpArrow.tif')
+left_arrow = cv2.imread(image_path + '/LeftArrow.tif')
+right_arrow = cv2.imread(image_path + '/Right Arrow.tif')
+
 fourcc = cv2.VideoWriter_fourcc(*'jpeg')
 out = cv2.VideoWriter('output.mov', fourcc, 20.0, (320, 240))
 stream = urllib.request.urlopen('http://192.168.0.35/webcam.mjpeg')
@@ -59,14 +65,42 @@ while True:
         jpg = bytes[a:b + 2]
         bytes = bytes[b + 2:]
         frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-        frame = frame / 255
-        new_frame = np.array([frame])
-        if frame is not None:
-            cv2.imshow("prediction", new_frame[0])
+        normalized_frame = frame / 255
+        new_frame = np.array([normalized_frame])
         prediction = tf.argmax(y, 1)
         command_map = {0:"left",1:"up",2:"right"}
         command_index = prediction.eval(feed_dict={x: new_frame}, session=sess)[0]
         command = command_map[command_index]
+        key_image = None
+        if command == 'left':
+            key_image = left_arrow
+        elif command == 'up':
+            key_image = up_arrow
+        elif command == 'right':
+            key_image = right_arrow
+        if frame is not None:
+            
+            arrow_key_scale = 0.125
+            resized_image = cv2.resize(key_image, None, fx=arrow_key_scale, fy=arrow_key_scale,interpolation=cv2.INTER_CUBIC)
+            cv2.imshow("prediction", new_frame[0])
+            # Thresholding requires grayscale only, so that threshold only needs to happen in one dimension
+            img2gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+
+            # Create mask where anything greater than 240 bright is made super white (255) / selected
+            ret, mask = cv2.threshold(img2gray, 240, 255, cv2.THRESH_BINARY)
+
+            # TODO: understand how this copy-pasted OpenCV masking code works
+            mask_inv = cv2.bitwise_not(mask)  # invert the mask
+            rows, cols, channels = resized_image.shape  # get size of image
+            region_of_interest = frame[0:rows, 0:cols]
+            img1_bg = cv2.bitwise_and(region_of_interest, region_of_interest, mask=mask)  # ???
+            img2_fg = cv2.bitwise_and(resized_image, resized_image, mask=mask_inv)  # ???
+            dst = cv2.add(img1_bg, img2_fg)  # ???
+            frame[0:rows, 0:cols] = dst
+
+            # Finally, show image with the an overlay of identified target key image
+            cv2.imshow('frame', frame)
+
         now = datetime.now()
         post_map = {"left": 37, "up": 38, "right": 39}
         post_command = post_map[command]
