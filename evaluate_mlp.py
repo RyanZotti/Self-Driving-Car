68f47269fe8679edab4f939b0e972795875cd303
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-09-12 08:59:49 -0400

    tests the quality of a trained mlp on entire validation set

---
+++ evaluate_mlp.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import urllib.request
+import requests
+import numpy as np
+from datetime import datetime
+import cv2
+import json
+
+sess = tf.InteractiveSession(config=tf.ConfigProto())
+
+def abc():
+    pass
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
+
+x = tf.placeholder(tf.float32, shape=[None, 240, 320, 3])
+y_ = tf.placeholder(tf.float32, shape=[None, 3])
+
+x_shaped = tf.reshape(x, [-1, 240 * 320 * 3])
+
+W1 = weight_variable([240 * 320 * 3, 32])
+b1 = bias_variable([32])
+h1 = tf.sigmoid(tf.matmul(x_shaped, W1) + b1)
+
+W2 = weight_variable([32, 3])
+b2 = bias_variable([3])
+y=tf.nn.softmax(tf.matmul(h1, W2) + b2)
+
+saver = tf.train.Saver()
+saver.restore(sess, "/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/tf_visual_data/runs/1/trained_model/model.ckpt")
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess.run(tf.initialize_all_variables())
+
+npzfile = np.load("/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/final_processed_data_3_channels.npz")
+
+# training data
+train_predictors = npzfile['train_predictors']
+train_targets = npzfile['train_targets']
+
+# validation/test data
+validation_predictors = npzfile['validation_predictors']
+validation_targets = npzfile['validation_targets']
+#validation_predictors, validation_targets = shuffle_dataset(validation_predictors, validation_targets)
+
+print("validation accuracy %g" % accuracy.eval(feed_dict={x: validation_predictors/255, y_: validation_targets}))
+
+print("Finished")
