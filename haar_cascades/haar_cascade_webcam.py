911cc2f8380c2b2b87675a17b164dcaa60ee9883
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-09-13 18:33:48 -0400

    bound stopsign with square instead of rectangle

--- haar_cascades/haar_cascade_webcam.py
+++ haar_cascades/haar_cascade_webcam.py
@@ -1,18 +1,30 @@
-import numpy
 import cv2
+
 
 def detect(img):
     cascade = cv2.CascadeClassifier("/Users/ryanzotti/Documents/repos/Self-Driving-Car/haar_cascades/classifier/cascade.xml")
     rects = cascade.detectMultiScale(img, 1.3, 4, 0, (20,20))
-
+    print(rects)
     if len(rects) == 0:
         return [], img
     rects[:, 2:] += rects[:, :2]
     return rects, img
 
+
+def make_int(number):
+    if number % 2 != 0:
+        number = number - 1
+    return int(number)
+
+
 def box(rects, img):
     for x1, y1, x2, y2 in rects:
-        cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+        y_diff = make_int(y1 - y2)
+        x_average = make_int((x1+x2)/2)
+        new_x1 = x_average - make_int((y_diff/2))
+        new_x2 = x_average + make_int((y_diff/2))
+        # last arguement is the thickness of the bounding box
+        cv2.rectangle(img, (new_x1, y1), (new_x2, y2), (127, 255, 0), 2)
     #cv2.imwrite('one.jpg', img);
 
 cap = cv2.VideoCapture(0)
