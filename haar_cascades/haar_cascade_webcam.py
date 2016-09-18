5f1fa7186b9a33f11ab48f777a3650567175d74e
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-09-18 16:49:26 -0400

    show only smallest concentric square

--- haar_cascades/haar_cascade_webcam.py
+++ haar_cascades/haar_cascade_webcam.py
@@ -17,15 +17,37 @@ def make_int(number):
     return int(number)
 
 
+def smallest_box(raw_squares):
+    index_smallest_square = 0
+    print(raw_squares)
+    if len(raw_squares) > 1:
+        # Assumes you'll only ever see one set of concentric squares, which won't
+        # always be true. Would need to test for different square centers
+        i = 0
+        for x1, y1, x2, y2 in raw_squares:
+            x1s = raw_squares[index_smallest_square][0]
+            y1s = raw_squares[index_smallest_square][1]
+            x2s = raw_squares[index_smallest_square][2]
+            y2s = raw_squares[index_smallest_square][3]
+            if x1 > x2s and y1 < y1s and x2 < x2s and y2 < y2s:
+                index_smallest_square = i
+    return index_smallest_square
+
+
 def box(rects, img):
+    index_smallest_square = smallest_box(rects)
+    i = 0
     for x1, y1, x2, y2 in rects:
         y_diff = make_int(y1 - y2)
-        x_average = make_int((x1+x2)/2)
-        new_x1 = x_average - make_int((y_diff/2))
-        new_x2 = x_average + make_int((y_diff/2))
-        # last arguement is the thickness of the bounding box
-        cv2.rectangle(img, (new_x1, y1), (new_x2, y2), (127, 255, 0), 2)
-    #cv2.imwrite('one.jpg', img);
+        x_average = make_int((x1 + x2) / 2)
+        new_x1 = x_average - make_int((y_diff / 2))
+        new_x2 = x_average + make_int((y_diff / 2))
+        if i == index_smallest_square:
+            # last arguement is the thickness of the bounding box
+            cv2.rectangle(img, (new_x1, y1), (new_x2, y2), (127, 255, 0), 2)
+        else:
+            cv2.rectangle(img, (new_x1, y1), (new_x2, y2), (0, 0, 255), 2)
+        i = i + 1
 
 cap = cv2.VideoCapture(0)
 cap.set(3,400)
