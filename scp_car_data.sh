76dbdce08707ed4d1eaa1fcfc05b9a2aad0f871e
Ryan Zotti <ryanzotti@ryans-mbp.attlocal.net>
2017-05-21 12:59:15 -0700

    Parameterize scp script

--- scp_car_data.sh
+++ scp_car_data.sh
@@ -1,3 +1,5 @@
-SESSION=$1
-scp pi@192.168.0.35:/home/pi/Documents/code/Self_Driving_RC_Car/data/${SESSION}/* /Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/data/${SESSION}/
+IP=$1
+SESSION=$2
+# Example: sh scp_car_data.sh 192.168.1.82 152
+scp pi@${IP}:/home/pi/Documents/code/Self_Driving_RC_Car/data/${SESSION}/* /Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/data/${SESSION}/
 echo "Finished"
