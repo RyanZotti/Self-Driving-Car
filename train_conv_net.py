d077775d0a0acc15bddc1bb02e6e887ea0f2ce56
Ryan Zotti <ryanzotti@ryans-mbp.attlocal.net>
2017-05-28 14:42:06 -0700

    Standardize convnet training

--- train_conv_net.py
+++ train_conv_net.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
-import numpy as np
 import argparse
-from util import (mkdir_tfboard_run_dir,mkdir,shell_command,
-                  shuffle_dataset, dead_ReLU_pct, custom_summary)
-from data_augmentation import flip_enrichment, normalize_contrast
-import os
+from Trainer import Trainer
 
 '''
 Helpful notes
@@ -23,40 +19,15 @@ Helpful notes
 # GPU: python train_conv_net.py -p /root/data -b 1000
 # Laptop: python train_conv_net.py -p /Users/ryanzotti/Documents/repos/Self_Driving_RC_Car -b 1000 -f y -d y
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--datapath", required = False,
+ap.add_argument("-d", "--datapath", required = False,
     help = "path to all of the data",
     default='/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car')
-ap.add_argument("-b", "--batches", required = False,
+ap.add_argument("-e", "--epochs", required = False,
     help = "quantity of batch iterations to run",
-    default='1000')
-ap.add_argument("-f", "--flip", required = False,
-    help = "Increase training size by flipping left and right images",
-    default='y')
-ap.add_argument("-d", "--debug", required = False,
-    help = "Print more details for easier debugging",
-    default='n')
+    default='50')
 args = vars(ap.parse_args())
 data_path = args["datapath"]
-batch_iterations = int(args["batches"])
-perform_image_flipping = True if args["flip"].lower() == 'y' else False
-debug_mode = True if args["debug"].lower() == 'y' else False
-
-input_file_path = data_path+'/final_processed_data_3_channels.npz'
-tfboard_basedir = mkdir(data_path+'/tf_visual_data/runs/')
-tfboard_run_dir = mkdir_tfboard_run_dir(tfboard_basedir)
-model_checkpoint_path = mkdir(tfboard_run_dir+'/trained_model')
-
-npzfile = np.load(input_file_path)
-
-# training data
-train_predictors = npzfile['train_predictors']
-train_targets = npzfile['train_targets']
-
-# validation/test data
-validation_predictors = npzfile['validation_predictors']
-validation_targets = npzfile['validation_targets']
-validation_predictors, validation_targets = shuffle_dataset(validation_predictors, validation_targets)
-
+epochs = args["epochs"]
 
 sess = tf.InteractiveSession(config=tf.ConfigProto())
 
@@ -84,7 +55,6 @@ h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
 dead_ReLUs1 = tf.placeholder(tf.float32,shape=[1])
 h_pool1 = max_pool_2x2(h_conv1)
 
-
 W_conv2 = weight_variable([6, 6, 16, 4])
 b_conv2 = bias_variable([4])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
@@ -109,8 +79,8 @@ b_fc1 = bias_variable([4])
 h_pool4_flat = tf.reshape(h_pool4, [-1, 15 * 20 * 4])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool4_flat, W_fc1) + b_fc1)
 
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+dropout_keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, dropout_keep_prob)
 
 W_fc2 = weight_variable([4, 3])
 b_fc2 = bias_variable([3])
@@ -122,99 +92,9 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# My filters look random, but apparently other people get random-looking filters too (https://github.com/tensorflow/tensorflow/issues/908)
-W_conv1_transposed = tf.transpose(W_conv1, [3, 0, 1, 2]) # I think this means make the batch the first dimension
-W_conv1_visual_summary = tf.image_summary("W_conv1",W_conv1_transposed,max_images=1000)
-
-tf_ReLU_layers = [h_conv1]
-
-# To view graph: tensorboard --logdir=/Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/tf_visual_data/runs/1/
-tf.scalar_summary('accuracy', accuracy)
-merged = tf.merge_all_summaries()
-
-
-train_dir = mkdir(tfboard_run_dir+"/trn/convnet/")
-train_activation_layer_1 = mkdir(tfboard_run_dir+"/trn_actvtn_lyr1")
-train_activation_layer_2 = mkdir(tfboard_run_dir+"/trn_actvtn_lyr2")
-train_activation_layer_3 = mkdir(tfboard_run_dir+"/trn_actvtn_lyr3")
-train_activation_layer_4 = mkdir(tfboard_run_dir+"/trn_actvtn_lyr4")
-
-validation_dir = mkdir(tfboard_run_dir+"/vld/convnet/")
-
-# Archive this script to document model design in event of good results that need to be replicated
-model_file_path = os.path.dirname(os.path.realpath(__file__))+'/'+os.path.basename(__file__)
-shell_command('cp {model_file} {archive_path}'.format(model_file=model_file_path,archive_path=tfboard_run_dir+'/'))
-
-train_writer = tf.train.SummaryWriter(train_dir,sess.graph)
-validation_writer = tf.train.SummaryWriter(validation_dir,sess.graph)
-
-# Data augmentation
-train_predictors = normalize_contrast(train_predictors)
-validation_predictors = normalize_contrast(validation_predictors)
-
-validation_predictors[:200] = validation_predictors[:200] / 255
-
-sess.run(tf.initialize_all_variables())
-batch_index = 0
-batches_per_epoch = (train_predictors.shape[0] - train_predictors.shape[0] % 50)/50
-for i in range(batch_iterations):
-
-    # Shuffle in the very beginning and after each epoch
-    if batch_index % batches_per_epoch == 0:
-        train_predictors, train_targets = shuffle_dataset(train_predictors, train_targets)
-        batch_index = 0
-    batch_index += 1
-
-    data_index = batch_index * 50
-    predictors = train_predictors[data_index:data_index+50]
-    target = train_targets[data_index:data_index+50]
-
-    if perform_image_flipping and debug_mode:
-        pre_flip_count = len(target)
-        predictors, target = flip_enrichment(predictors,target)
-        post_flip_count = len(target)
-        msg = 'Pre-flip count: {0}, post-flip count: {1}'.format(pre_flip_count,post_flip_count)
-        print(msg)
-
-    predictors = predictors / 255
-
-    if i%425 == 0:
-
-        # Not sure what these two lines do
-        run_opts = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-        run_opts_metadata = tf.RunMetadata()
-        train_feed = {x: predictors, y_: target, keep_prob: 1.0}
-        train_summary, train_accuracy = sess.run([merged, accuracy],
-                              feed_dict=train_feed,
-                              options=run_opts,
-                              run_metadata=run_opts_metadata)
-        train_writer.add_run_metadata(run_opts_metadata, 'step%03d' % i)
-        train_writer.add_summary(train_summary, i)
-
-        validation_feed = {x: validation_predictors[:200], y_: validation_targets[:200], keep_prob: 1.0}
-        validation_summary, validation_accuracy = sess.run([merged, accuracy],
-                                                 feed_dict=validation_feed,
-                                                 options=run_opts,
-                                                 run_metadata=run_opts_metadata)
-        validation_writer.add_run_metadata(run_opts_metadata, 'step%03d' % i)
-        validation_writer.add_summary(validation_summary, i)
-
-        ReLU_layers = sess.run(tf_ReLU_layers,feed_dict=train_feed)
-        for layer_index, ReLU_layer in enumerate(ReLU_layers):
-            dead_ReLU_percentage = dead_ReLU_pct(ReLU_layer)
-            relu_summary_str = "dead_ReLUs_layer"+str(layer_index+1)
-            summary = custom_summary(relu_summary_str,dead_ReLU_percentage)
-            train_writer.add_summary(summary,i)
-
-        print("{i} training accuracy: {train_acc}, validation accuracy: {validation_acc}".format(train_acc=train_accuracy,validation_acc=validation_accuracy,i=i))
-
-
-    train_step.run(feed_dict={x: predictors, y_: target, keep_prob: 0.5})
-
-# Save the trained model to a file
-saver = tf.train.Saver()
-save_path = saver.save(sess, model_checkpoint_path+"/model.ckpt")
-#print("validation accuracy %g" % accuracy.eval(feed_dict={x: validation_predictors, y_: validation_targets, keep_prob: 1.0}))
-
-# Marks unambiguous successful completion to prevent deletion by cleanup script
-shell_command('touch '+tfboard_run_dir+'/SUCCESS')
+trainer = Trainer(data_path=data_path, epochs=epochs, max_sample_records=250)
+trainer.train(sess=sess, x=x, y_=y_,
+              accuracy=accuracy,
+              train_step=train_step,
+              train_feed_dict={dropout_keep_prob:50},
+              test_feed_dict={dropout_keep_prob:100})
