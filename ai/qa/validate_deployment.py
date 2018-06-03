6c186a9ff11d0cfdb3c0d4902471e1125ee0b051
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2018-06-03 13:29:30 -0700

    Parameterized deployment validation

--- ai/qa/validate_deployment.py
+++ ai/qa/validate_deployment.py
@@ -25,15 +25,29 @@ ap.add_argument(
     required=False,
     help="Where to find a single image to compare predictions",
     default='/Users/ryanzotti/Documents/Data/Self-Driving-Car/printer-paper/data/dataset_2_18-04-15/record_845.json')
+ap.add_argument(
+    "--port",
+    required=False,
+    help="Serer port to use",
+    default=8888)
 
 args = vars(ap.parse_args())
 checkpoint_dir = args['checkpoint_dir']
 data_path = args['data_path']
 image_scale = args['image_scale']
 label_path = args['label_path']
+port=args['port']
 
 # Run prediction API as a subprocess
-cmd = 'python ../prediction_api.py'
+cmd = '''
+    python ../prediction_api.py \
+        --checkpoint_dir {checkpoint_dir} \
+        --image_scale {image_scale} \
+        --port {port}
+    '''.format(
+        checkpoint_dir=checkpoint_dir,
+        image_scale=image_scale,
+        port=port)
 api_process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
 # Load model just once and store in memory for all future calls
