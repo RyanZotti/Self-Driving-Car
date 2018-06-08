6d55838f8fe6e65d089847ca137d6c19a005e3d0
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2018-06-08 14:54:33 -0400

    Remove dead code

--- car/parts/web_controller/web.py
+++ car/parts/web_controller/web.py
@@ -19,79 +19,9 @@ import tornado.ioloop
 import tornado.web
 import tornado.gen
 
-from car import utils
-
-
-class RemoteWebServer():
-    '''
-    A controller that repeatedly polls a remote webserver and expects
-    the response to be angle, throttle and drive mode.
-    '''
-
-    def __init__(self, remote_url, connection_timeout=.25):
-
-        self.control_url = remote_url
-        self.time = 0.
-        self.angle = 0.
-        self.throttle = 0.
-        self.mode = 'user'
-        self.recording = False
-        # use one session for all requests
-        self.session = requests.Session()
-
-    def update(self):
-        '''
-        Loop to run in separate thread the updates angle, throttle and
-        drive mode.
-        '''
-
-        while True:
-            # get latest value from server
-            self.angle, self.throttle, self.mode, self.recording = self.run()
-
-    def run_threaded(self):
-        '''
-        Return the last state given from the remote server.
-        '''
-
-        # return last returned last remote response.
-        return self.angle, self.throttle, self.mode, self.recording
-
-    def run(self):
-        '''
-        Posts current car sensor data to webserver and returns
-        angle and throttle recommendations.
-        '''
-
-        data = {}
-        response = None
-        while response == None:
-            try:
-                response = self.session.post(self.control_url,
-                                             files={'json': json.dumps(data)},
-                                             timeout=0.25)
-
-            except (requests.exceptions.ReadTimeout) as err:
-                print("\n Request took too long. Retrying")
-                # Lower throttle to prevent runaways.
-                return self.angle, self.throttle * .8, None
-
-            except (requests.ConnectionError) as err:
-                # try to reconnect every 3 seconds
-                print("\n Vehicle could not connect to server. Make sure you've " +
-                      "started your server and you're referencing the right port.")
-                time.sleep(3)
-
-        data = json.loads(response.text)
-        angle = float(data['angle'])
-        throttle = float(data['throttle'])
-        drive_mode = str(data['drive_mode'])
-        recording = bool(data['recording'])
-
-        return angle, throttle, drive_mode, recording
-
 
 class LocalWebController(tornado.web.Application):
+
     def __init__(self):
         '''
         Create and publish variables needed on many of
