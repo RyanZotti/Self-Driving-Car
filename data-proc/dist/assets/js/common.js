8dc9c09dba361e3406a6d9fd723742a2d6608de9
Ryan Zotti <ryanzotti@ryans-mbp.gateway.sonic.net>
2018-11-08 16:05:32 -0800

    Create common / shared js script

---
+++ data-proc/dist/assets/js/common.js
@@ -0,0 +1,7 @@
+// https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
--- data-proc/src/assets/js/editor.js
+++ data-proc/src/assets/js/editor.js
@@ -1,11 +1,3 @@
-// https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
-function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return template.content.firstChild;
-}
-
 function getDatasetImportRowString() {
     return new Promise(function(resolve, reject) {
         $.get( "/dataset-import.html", function(datasetString) {
