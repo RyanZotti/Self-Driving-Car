91d493c93f756b6f3e6b2517c4d38d40107529c5
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2019-01-01 13:54:14 -0800

    Read and write Raspberry Pi fields from UI

--- data-proc/dist/assets/js/raspberrypi.js
+++ data-proc/dist/assets/js/raspberrypi.js
@@ -21,3 +21,25 @@ function readPiField(fieldName) {
         });
     });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const raspberryPiFields = document.querySelectorAll('input.raspberry-pi-field');
+    for (const field of raspberryPiFields){
+        const columnName = field.getAttribute("column-name");
+        field.onchange = function(){
+            const columnValue = field.value;
+            writePiField(
+                columnName,
+                columnValue
+            );
+        }
+        const fieldType = field.getAttribute('type');
+        if (fieldType.toLowerCase() != 'password'){
+            readPiField(columnName).then(function(fieldValue){
+                field.value = fieldValue;
+            });
+        }
+    }
+
+}, false);
