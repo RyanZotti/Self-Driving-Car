870a828b73725d567235ab74b43e92dca1fa0af5
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2019-02-01 18:47:57 -0600

    Remove scp_car_data.sh

--- scp_car_data.sh
+++
@@ -1,5 +0,0 @@
-IP=$1
-SESSION=$2
-# Example: sh scp_car_data.sh 192.168.1.82 152
-scp pi@${IP}:/home/pi/Documents/code/Self_Driving_RC_Car/data/${SESSION}/* /Users/ryanzotti/Documents/repos/Self_Driving_RC_Car/data/${SESSION}/
-echo "Finished"
