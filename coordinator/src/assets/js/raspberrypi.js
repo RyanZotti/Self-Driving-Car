2b2d7514450da8135e7c364a4b348ac3ea6bd02d
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2019-02-18 20:08:53 -0800

    Add src/raspberrypi.js

---
+++ coordinator/src/assets/js/raspberrypi.js
@@ -0,0 +1,93 @@
+function writePiField(fieldName, fieldValue) {
+    return new Promise(function(resolve, reject) {
+        const input = JSON.stringify({
+          'column_name': fieldName,
+          'column_value': fieldValue
+        });
+        $.post('/write-pi-field', input, function(){
+            resolve();
+        });
+    });
+}
+
+function readPiField(fieldName) {
+    return new Promise(function(resolve, reject) {
+        const input = JSON.stringify({
+          'column_name': fieldName
+        });
+        $.post('/read-pi-field', input, function(output){
+            resolve(output['column_value']);
+        });
+    });
+}
+
+async function setEndpointText(){
+    const promises = [
+        readPiField("username"),
+        readPiField("hostname")
+    ];
+    endpointComponents = await Promise.all(promises);
+    const userName = endpointComponents[0];
+    const hostName = endpointComponents[1];
+    const endpointText = document.querySelector('output[name="endpoint"]');
+    endpointText.textContent = userName + '@' + hostName;
+}
+
+function resetCheckStatusButton(){
+    const testPiConnectionButton = document.querySelector('button#test-pi-connection-button');
+    testPiConnectionButton.classList.remove('btn-warning');
+    testPiConnectionButton.classList.remove('btn-danger');
+    testPiConnectionButton.classList.remove('btn-success');
+    testPiConnectionButton.classList.add('btn-primary');
+    testPiConnectionButton.textContent = 'Test Connection';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const raspberryPiFields = document.querySelectorAll('input.raspberry-pi-field');
+    for (const field of raspberryPiFields){
+        const columnName = field.getAttribute("column-name");
+        field.onchange = async function(){
+            const columnValue = field.value;
+            await writePiField(
+                columnName,
+                columnValue
+            );
+            setEndpointText();
+        }
+        const fieldType = field.getAttribute('type');
+        if (fieldType.toLowerCase() != 'password'){
+            readPiField(columnName).then(function(fieldValue){
+                field.value = fieldValue;
+            });
+        }
+    }
+    setEndpointText();
+
+    // Update Raspberry Pi statues
+    const piHealthCheckTime = setInterval(function(){
+        updatePiConnectionStatuses()
+    }, 1000);
+
+    const testPiConnectionButton = document.querySelector('button#test-pi-connection-button');
+    testPiConnectionButton.onclick = async function(){
+        this.textContent = 'Checking...';
+        this.classList.remove('btn-primary');
+        this.classList.remove('btn-danger');
+        this.classList.remove('btn-success');
+        this.classList.add('btn-warning');
+        const isHealthy = await raspberryPiConnectionTest();
+        if (isHealthy){
+            this.classList.remove('btn-warning');
+            this.classList.add('btn-success');
+            this.textContent = 'Connection Succeeded!';
+        } else {
+            this.classList.remove('btn-warning');
+            this.classList.add('btn-danger');
+            this.textContent = 'Connection Failed!';
+        }
+        const resetTimer = setTimeout(resetCheckStatusButton, 3000);
+    }
+
+
+}, false);
