8a8d30ed5f04589000e032ec0e7e2769ec24b570
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2019-02-18 20:07:51 -0800

    Update src/machine-learing.js

--- coordinator/src/assets/js/machine-learning.js
+++ coordinator/src/assets/js/machine-learning.js
@@ -15,7 +15,7 @@ function addDatasetMachineLearningRows() {
         const datasetRowString = promiseResults[0];
         const datasetPromises = promiseResults[1];
         const tbody = document.querySelector("tbody#datasetsTbody");
-        for (datsetPromise of datasetPromises) {
+        for (const datsetPromise of datasetPromises) {
             const tr = htmlToElement(datasetRowString);
             datsetPromise.then(function(dataset){
                 const datasetText = dataset.name;
@@ -24,15 +24,54 @@ function addDatasetMachineLearningRows() {
 
                 // Make sure train select buttons are functional
                 const trainSelectInput = tr.querySelector('input[name="trainSelect"]');
-                trainSelectInput.setAttribute('id','dataset-id-'+dataset.id);
+                trainSelectInput.setAttribute('id','train-dataset-id-'+dataset.id);
+                trainSelectInput.setAttribute('dataset',datasetText);
                 const trainSelectLabel = tr.querySelector('label[name="trainSelect"]');
-                trainSelectLabel.setAttribute('for','dataset-id-'+dataset.id);
+                trainSelectLabel.setAttribute('for','train-dataset-id-'+dataset.id);
+                trainSelectInput.onclick = function(){
+                    console.log('train '+datasetText+' '+this.checked);
+                    const input = JSON.stringify({
+                        'web_page': 'machine learning',
+                        'name': 'training dataset',
+                        'detail': datasetText,
+                        'is_on': this.checked
+                    });
+                    writeToggle(input);
+                }
+                const trainingToggleReadInput = JSON.stringify({
+                    'web_page': 'machine learning',
+                    'name': 'training dataset',
+                    'detail': datasetText
+                });
+                readToggle(trainingToggleReadInput).then(function(is_on) {
+                    trainSelectInput.checked = is_on;
+                });
+
 
                 // Make sure validation select buttons are functional
                 const validationSelectInput = tr.querySelector('input[name="validationSelect"]');
-                validationSelectInput.setAttribute('id','dataset-id-'+dataset.id);
+                validationSelectInput.setAttribute('id','validation-dataset-id-'+dataset.id);
+                validationSelectInput.setAttribute('dataset',datasetText);
                 const validationSelectLabel = tr.querySelector('label[name="validationSelect"]');
-                validationSelectLabel.setAttribute('for','dataset-id-'+dataset.id);
+                validationSelectLabel.setAttribute('for','validation-dataset-id-'+dataset.id);
+                validationSelectInput.onclick = function(){
+                    console.log('validation '+datasetText+' '+this.checked);
+                    const input = JSON.stringify({
+                        'web_page': 'machine learning',
+                        'name': 'validation dataset',
+                        'detail': datasetText,
+                        'is_on': this.checked
+                    });
+                    writeToggle(input);
+                }
+                const validationToggleReadInput = JSON.stringify({
+                    'web_page': 'machine learning',
+                    'name': 'validation dataset',
+                    'detail': datasetText
+                });
+                readToggle(validationToggleReadInput).then(function(is_on) {
+                    validationSelectInput.checked = is_on;
+                });
 
                 tbody.appendChild(tr);
             });
@@ -56,7 +95,216 @@ function selectAllMachineLearningDatasetsTrigger(){
     }
 };
 
+function stopTraining() {
+    return new Promise(function(resolve, reject){
+        $.post('/stop-training', function(result){
+            resolve(result.is_running);
+        });
+    });
+}
+
+// TODO: Allow transfer learning
+function startTraining() {
+    return new Promise(async function(resolve, reject){
+        const trainDatasets = getMLCheckedDatasets('train');
+        const validationDatasets = getMLCheckedDatasets('validation');
+        const exists = await doesModelExist();
+        if (exists == true){
+            $.post('/resume-training', function(result){
+                resolve(result);
+            });
+        } else {
+            $.post('/train-new-model', function(result){
+                resolve(result);
+            });
+        }
+    });
+}
+
+/*
+Used to check if model API train a model from
+scratch or apply transfer learning to an existing
+model
+*/
+function doesModelExist() {
+    return new Promise(function(resolve, reject){
+        $.post('/does-model-already-exist', function(result){
+            resolve(result.exists);
+        });
+    });
+}
+
+function laptoModelApiHealth() {
+    return new Promise(function(resolve, reject){
+        $.post('/laptop-model-api-health', function(result){
+            resolve(result['process_id']);
+        });
+    });
+}
+
+function deployModelLaptop() {
+    return new Promise(function(resolve, reject){
+        $.post('/deploy-laptop-model', function(result){
+            resolve(result);
+        });
+    });
+}
+
+/*
+The list of processes should be the source of
+truth regarding the current state of training.
+It's better to look up proceesses than to save
+the state in a variable somewhere, especially
+if users are going to switch screens frequently
+*/
+function isTraining() {
+    return new Promise(function(resolve, reject){
+        $.post('/is-training', function(result){
+            resolve(result.is_running);
+        });
+    });
+}
+
+function setTrainButtonState() {
+    if (isAttemptingTrainingStop == false){
+        const trainModelButton = document.querySelector("button#train-model-button");
+        isTraining().then(function(processExists){
+            isTrainingLastState = processExists;
+            if(processExists == true){
+                trainModelButton.textContent = 'Stop Training'
+                if(trainModelButton.classList.contains("btn-primary")){
+                     trainModelButton.classList.remove("btn-primary");
+                }
+                if(!trainModelButton.classList.contains("btn-danger")){
+                    trainModelButton.classList.add("btn-danger");
+                }
+            } else {
+                trainModelButton.textContent = 'Start Training'
+                if(trainModelButton.classList.contains("btn-danger")){
+                    trainModelButton.classList.remove("btn-danger");
+                }
+                if(!trainModelButton.classList.contains("btn-primary")){
+                    trainModelButton.classList.add("btn-primary");
+                }
+            }
+        });
+    };
+}
+
+function getNewEpochs(modelId) {
+    return new Promise(function(resolve, reject) {
+        data = JSON.stringify({'model_id': modelId})
+        $.post('/get-new-epochs', data, function(result){
+           resolve(result['epochs'])
+        });
+    });
+}
+
+function checkIfEpochAlreadyInTable(epoch){
+    var exists = false;
+    const epochsTable = document.querySelector("tbody#epochs-tbody");
+    const rows = epochsTable.querySelectorAll("tr");
+    for (const row of rows){
+        const epochTd = row.querySelector('td.epoch-id');
+        const existingEpochId = epochTd.textContent;
+        if (epoch == existingEpochId){
+            exists = true;
+            break;
+        }
+    }
+    return exists;
+}
+
+async function fillEpochsTable(modelId){
+    const epochs = await getNewEpochs(modelId);
+    const epochsTable = document.querySelector("tbody#epochs-tbody");
+    for (epoch of epochs){
+        const row = await getHtml("machine-learning-epoch.html");
+        const rowExists = checkIfEpochAlreadyInTable(epoch['epoch']);
+        if (rowExists != true){
+            const epochIdTd = row.querySelector("td.epoch-id");
+            epochIdTd.textContent = epoch['epoch'];
+            const trainingTd = row.querySelector("td.epoch-train");
+            trainingTd.textContent = (epoch['train']).toFixed(2);
+            const validationTd = row.querySelector("td.epoch-validation");
+            validationTd.textContent = (epoch['validation']).toFixed(2);
+            epochsTable.appendChild(row);
+        }
+    }
+}
+
+function getMLCheckedDatasets(datasetType){
+    const rows = document.querySelectorAll("tbody#datasetsTbody > tr");
+    const checkedDatasets = [];
+    const inputName = datasetType.toLowerCase() + 'Select';
+    for (const row of rows){
+        const checkBox = row.querySelector('input[name="'+inputName+'"]');
+        const dataset = checkBox.getAttribute('dataset');
+        if (checkBox.checked == true){
+            checkedDatasets.push(dataset)
+        }
+    }
+    return checkedDatasets;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     selectAllMachineLearningDatasetsTrigger();
     addDatasetMachineLearningRows();
+    var modelId = 1;
+    fillEpochsTable(modelId)
+    /*
+    The training could complete successfully or fail at any
+    time, so make sure to check it every 5 seconds. The time
+    loop can be quit with a call to clearInterval(<timevar>);
+    */
+    const trainingStateTimer = setInterval(function(){
+      setTrainButtonState();
+      // TODO: Remove hardcoded modelId
+      var modelId = 1;
+      fillEpochsTable(modelId);
+    }, 1000);
+
+    // Update Raspberry Pi statues
+    const piHealthCheckTime = setInterval(function(){
+        updatePiConnectionStatuses()
+    }, 1000);
+
+    const trainModelButton = document.querySelector("button#train-model-button");
+    trainModelButton.onclick = function(){
+        if(isTrainingLastState == true){
+            const trainModelButton = document.querySelector("button#train-model-button");
+            isAttemptingTrainingStop = true;
+            trainModelButton.textContent = 'Stopping Training ...'
+            stopTraining().then(function(){
+                setTrainButtonState();
+                isAttemptingTrainingStop = false;
+            });
+        } else {
+            startTraining().then(function(){
+                setTrainButtonState();
+            });
+        }
+    };
+
+    // TODO: Figure out how to differentiate from Pi model deployment
+    const deployModelButton = document.querySelector("button#deploy-model-laptop-button");
+    deployModelButton.onclick = function(){
+        deployModelLaptop();
+    }
+
+    const trackedToggles = document.querySelectorAll('input.tracked-toggle');
+    for (const toggle of trackedToggles){
+        configureToggle(toggle);
+    }
+
 }, false);
+
+var isTrainingLastState = false;
+
+/*
+Used to ensure that the train button doesn't go
+from "stop training..." to "stop training" briefly
+as this might cause users to double click and
+accidentally start training again
+*/
+var isAttemptingTrainingStop = false;
