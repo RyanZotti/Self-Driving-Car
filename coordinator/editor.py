4dfccb62c612ee57a42cdcccb29c20af8401d315
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2020-03-15 12:10:34 -0700

    Fix dataset typo

--- coordinator/editor.py
+++ coordinator/editor.py
@@ -2402,7 +2402,7 @@ class GetPiDatasetName(tornado.web.RequestHandler):
         async with ClientSession(timeout=timeout) as session:
             async with session.get(endpoint) as response:
                 dataset = await response.json()
-                return {'dataset': -dataset}
+                return {'dataset': dataset}
 
 class CreateNewDataset(tornado.web.RequestHandler):
 
